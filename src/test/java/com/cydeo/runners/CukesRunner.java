5cb8e3061243cb81b48e8c778ba7554a598d3ca0
CrazyDevo <adam@cybertekschool.com>
2022-03-19 11:53:38 -0400

    CukesRunner is created

--- src/test/java/com/cydeo/runners/CukesRunner.java
+++ src/test/java/com/cydeo/runners/CukesRunner.java
@@ -7,13 +7,16 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
+        plugin = "html:target/cucumber-report.html",
         //in order to get the path of definitions
         glue = "com/cydeo/step_definitions",
         //in order to get the path of feature files
         features = "src/test/resources/features",
         //if true we can get the snippits which are unimplemented
         //if false we can run the methods
-        dryRun = true
+        dryRun = true,
+        tags ="@wip"
+
 
 
 
